50c8cba36fcd9bd8bfa8c728735649e5a4161640
Jared Hanson <jaredhanson@gmail.com>
2019-06-27 13:30:29 -0700

    Remove unused handler.

--- handlers/login.js
+++
@@ -1,9 +0,0 @@
-var passport = require('passport');
-
-
-exports = module.exports = [
-  passport.authenticate('local', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
-  }
-];
