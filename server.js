3b89bda22d0ca06ef987bc2a7d6d7bb9a6e64301
Márton László Attila <emela@emela.hu>
2021-03-06 09:22:23 +0100

    Add console.log, when view req.body

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ var db = require('./db');
 // that the password is correct and then invoke `cb` with a user object, which
 // will be set at `req.user` in route handlers after authentication.
 passport.use(new Strategy(
-  function(username, password, cb) {
-    db.users.findByUsername(username, function(err, user) {
+  function (username, password, cb) {
+    db.users.findByUsername(username, function (err, user) {
       if (err) { return cb(err); }
       if (!user) { return cb(null, false); }
       if (user.password != password) { return cb(null, false); }
@@ -28,11 +28,11 @@ passport.use(new Strategy(
 // typical implementation of this is as simple as supplying the user ID when
 // serializing, and querying the user record by ID from the database when
 // deserializing.
-passport.serializeUser(function(user, cb) {
+passport.serializeUser(function (user, cb) {
   cb(null, user.id);
 });
 
-passport.deserializeUser(function(id, cb) {
+passport.deserializeUser(function (id, cb) {
   db.users.findById(id, function (err, user) {
     if (err) { return cb(err); }
     cb(null, user);
@@ -62,30 +62,31 @@ app.use(passport.session());
 
 // Define routes.
 app.get('/',
-  function(req, res) {
+  function (req, res) {
     res.render('home', { user: req.user });
   });
 
 app.get('/login',
-  function(req, res){
+  function (req, res) {
     res.render('login');
   });
-  
-app.post('/login', 
+
+app.post('/login',
   passport.authenticate('local', { failureRedirect: '/login' }),
-  function(req, res) {
+  function (req, res) {
+    console.log('This is request data --------->', req.body);
     res.redirect('/');
   });
-  
+
 app.get('/logout',
-  function(req, res){
+  function (req, res) {
     req.logout();
     res.redirect('/');
   });
 
 app.get('/profile',
   require('connect-ensure-login').ensureLoggedIn(),
-  function(req, res){
+  function (req, res) {
     res.render('profile', { user: req.user });
   });
 
